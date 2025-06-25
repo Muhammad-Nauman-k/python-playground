cbb2b2cdc1fc46c86425f93965cde6e81bcd207c
Muhammad Nauman <naumankn34@gmail.com>
2025-06-26 00:29:04 +0500

    Leap Year (Utilities) lec10)

---
+++ Utilities/leap_year.py
@@ -0,0 +1,23 @@
+# Leap Year
+
+year = int(input("Enter Year: "))
+
+def is_leap_year(year):
+    """Here if year/4 = 0 AND if year/100 != 0   OR   If year/4 = 0 AND if year/100 = 0 AND year/400 = 0 """
+    if year % 4 == 0:
+        if year % 100 != 0:
+            return True
+        else:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+    else:
+        return False
+
+result = is_leap_year(year)
+if result == True:
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
+            
