f277b6944bfcb8b85f908062530a967470ee5e46
Muhammad Nauman <naumankn34@gmail.com>
2025-06-29 17:20:09 +0500

    Updated Snake Game with file handling (Games)

--- Games/The Snake Game/scoreboard_file.py
+++ Games/The Snake Game/scoreboard_file.py
@@ -7,18 +7,32 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score_file()
         self.penup()
         self.hideturtle()
         self.goto(0,270)
         self.color("white")
-        self.write(f"Score: {self.score}",align=ALIGNMENT,font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}",align=ALIGNMENT,font=FONT)
         
-    def gameover(self):
-        self.goto(0,0)
-        self.write("Game Over ",align=ALIGNMENT,font=FONT_FOR_GAMEOVER)
+    def high_score_file(self):
+        with open("data.txt",mode="r") as file:
+            self.high_score = int(file.read())                        
+        
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            
+            with open("data.txt",mode="w") as file:
+                file.write(str(self.high_score))
+        
+        self.score = 0
+        self.update_score()
+    
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}",align=ALIGNMENT,font=FONT)
         
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}",align=ALIGNMENT,font=FONT)
+        self.update_score()
         
